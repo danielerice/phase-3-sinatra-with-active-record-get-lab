0050263453dec54a1598d34eccfd7e2d1fdadd3f
Daniel Rice <danielrice5627@gmail.com>
2023-03-27 17:52:04 -0700

    commit tree 2b1278aa25bf11a148a7b079cf13f08bfbfde451

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,30 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
+  get '/bakeries' do
 
-  # add routes
+      bakeries = Bakery.all
+  
+      bakeries.to_json
+    end
 
+    get '/bakeries/:id' do
+      
+      bakery = Bakery.find(params[:id])
+      
+      bakery.to_json(include: :baked_goods)
+    end
+
+    get '/baked_goods/by_price' do
+
+      baked_goods_by_price = BakedGood.all.order('price DESC')
+
+      baked_goods_by_price.to_json
+    end
+
+    get '/baked_goods/most_expensive' do
+
+      baked_goods_priciest = BakedGood.all.order('price DESC').limit(1)
+
+      baked_goods_priciest.to_json
+    end
 end
